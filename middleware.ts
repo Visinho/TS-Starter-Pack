3f5956486e93e612c81305a693620314d4fb7452
Visinho <Elvisvisinho@rocketmail.com>
2024-04-29 09:52:03 +0100

    app test with vitest

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,7 @@ export function notFound(req: Request, res: Response, next: NextFunction) {
 
 export function errorHandler(err: Error, req: Request, res: Response<ErrorMessage>, next: NextFunction) {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+    res.status(statusCode);
     res.json({
         message: err.message,
         stack: err.stack
